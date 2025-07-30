9e3231604f1e0f945b16714dfeb14456555d62d6
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-07-30 16:25:19 +0300

    designed the home page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { MdMarkEmailRead } from "react-icons/md";
 
 const Services = () => {
+  const HandleEmailSends=()=>{
+        window.location.href="https://mail.google.com/mail/?view=cm&fs=1&to=felixkiprotich2000@gmail.com&su=Thank you for reaching out to RR Events and Catering Solutions&body=Hello there.Thanks for reaching out to RR Events and Catering Solutions. How May I help you?",
+            "_blank", "_blank"
+    }
+
+    const handleCall=()=>{
+        window.location.href="tel:+254758364336"
+    }
   return (
     <div className='flex text-gray-700 p-5 flex-col w-full items-center justify-center h-screen'>
       <div className='flex w-full items-center justify-between'>
-        <button className='flex items-center justify-center gap-2 border border-gray-200 p-1 text-[12px] w-[100px] rounded-lg'><MdMarkEmailRead /> Send Email</button>
+        <button onClick={HandleEmailSends} className='flex items-center justify-center gap-2 border border-gray-200 p-1 text-[12px] w-[100px] rounded-lg'><MdMarkEmailRead /> Send Email</button>
         <h1 className='text-xl font-bold '>Our Services</h1>
-        <button className='fle w-[100px] bg-orange-500 text-white text-[12px] font-bold p-2 rounded-full shadow-sm'>Get in Touch</button>
+        <button onClick={handleCall} className='fle w-[100px] bg-orange-500 text-white text-[12px] font-bold p-2 rounded-full shadow-sm'>Get in Touch</button>
       </div>
     </div>
   )

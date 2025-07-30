405b93da47a1dc93738cf061f6a4df75ee4bc6da
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-07-30 16:41:13 +0300

    designed the home page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,11 +17,18 @@ const Services = () => {
         <h1 className='text-xl font-bold '>Our Services</h1>
         <button onClick={handleCall} className='flex items-center justify-center cursor-pointer w-[100px] bg-orange-500 text-white text-[12px] font-bold p-2 rounded-full shadow-sm'>Make a Call</button>
       </div>
+      <div className='flex w-full items-center '>
+        <p className='text-[14px] mt-4 '>
+          From Kitchen to Celebration, We Handle It All.
+<span className='text-orange-500 font-bold text-[16px] ml-2'>Weddings, birthdays, or corporate events </span> — we serve delicious meals, tents, décor, music, and full planning to bring your vision to life.
+        </p>
+        
+        </div>
       <div className='flex flex-col w-full mt-5 lg:grid lg:grid-cols-4 gap-5'>
-        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md h-[300px]'>section 1</div>
-        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md h-[300px]'>section 1</div>
-        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md h-[300px]'>section 1</div>
-        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md h-[300px]'>section 1</div>
+        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md rounded-xl h-[300px]'>section 1</div>
+        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md rounded-xl h-[300px]'>section 1</div>
+        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md rounded-xl h-[300px]'>section 1</div>
+        <div className='flex bg-gray-50 w-full items-center justify-center flex-col shadow-md rounded-xl h-[300px]'>section 1</div>
       </div>
     </div>
   )

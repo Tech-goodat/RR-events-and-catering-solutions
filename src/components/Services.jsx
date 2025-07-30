8807b42deeb8a1f0a71851052ec458b5764468d9
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-07-30 16:28:43 +0300

    designed the home page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,9 +13,9 @@ const Services = () => {
   return (
     <div className='flex text-gray-700 p-5 flex-col w-full items-center justify-center h-screen'>
       <div className='flex w-full items-center justify-between'>
-        <button onClick={HandleEmailSends} className='flex items-center justify-center gap-2 border border-gray-200 p-1 text-[12px] w-[100px] rounded-lg'><MdMarkEmailRead /> Send Email</button>
+        <button onClick={HandleEmailSends} className='flex cursor-pointer items-center justify-center gap-2 border border-gray-200 p-1 text-[12px] w-[100px] rounded-lg'><MdMarkEmailRead /> Send Email</button>
         <h1 className='text-xl font-bold '>Our Services</h1>
-        <button onClick={handleCall} className='fle w-[100px] bg-orange-500 text-white text-[12px] font-bold p-2 rounded-full shadow-sm'>Make a Call</button>
+        <button onClick={handleCall} className='flex items-center justify-center cursor-pointer w-[100px] bg-orange-500 text-white text-[12px] font-bold p-2 rounded-full shadow-sm'>Make a Call</button>
       </div>
     </div>
   )

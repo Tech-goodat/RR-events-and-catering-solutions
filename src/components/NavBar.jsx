ec1329f919d3baf9be1d581f662f381007bc3820
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-07-30 13:09:38 +0300

    set up responsive nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaHome } from "react-icons/fa";
 import { FaQuestionCircle } from "react-icons/fa";
 import { IoIosContacts } from "react-icons/io";
 import { FaServicestack } from "react-icons/fa6";
 import { MdOutlineTipsAndUpdates } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { FaBars } from "react-icons/fa";
 const NavBar = ({ activeSection }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
   const linkClass = (id) =>
     `rounded-full px-4 p-1.5 text-[12px]  hover:bg-orange-400 text-gray-700 flex gap-2 hover:text-white ${
       activeSection === id ? 'bg-orange-400 text-white' : ''
     }`;
 
   return (
-    <div className='fixed top-0 left-0 z-50 flex w-full items-center justify-between h-[50px] p-5 shadow-md bg-white'>
+    <div className='fixed  top-0 left-0 z-50 flex w-full items-center justify-between lg:justify-center h-[50px] p-5 shadow-md bg-white'>
+      <a href='#home' className="font-bold">Logo</a>
+      <div className='lg:flex w-full ml-5 hidden items-center justify-between'>
       <a href='#home' className={linkClass('home')}><FaHome size={20} />Home</a>
       <a href='#about' className={linkClass('about')}><FaQuestionCircle size={20}/>About</a>
       <a href='#contact' className={linkClass('contact')}><IoIosContacts size={20}/>Contact</a>
       <a href='#services' className={linkClass('services')}><FaServicestack size={20}/>Services</a>
       <a href='#tips' className={linkClass('tips')}><MdOutlineTipsAndUpdates size={20}/>Tips</a>
+
+      </div>
+      <button onClick={toggleMenu} className='lg:hidden text-gray-700'>
+        {isOpen ? <IoMdClose />:<FaBars />}
+      </button>
+      {isOpen && (
+        <div className='absolute top-12 left-0 w-full bg-white shadow-lg p-5 flex flex-col items-start'>
+          <a href='#home' className={linkClass('home')} onClick={toggleMenu}><FaHome size={20} />Home</a>
+          <a href='#about' className={linkClass('about')} onClick={toggleMenu}><FaQuestionCircle size={20}/>About</a>
+          <a href='#contact' className={linkClass('contact')} onClick={toggleMenu}><IoIosContacts size={20}/>Contact</a>
+          <a href='#services' className={linkClass('services')} onClick={toggleMenu}><FaServicestack size={20}/>Services</a>
+          <a href='#tips' className={linkClass('tips')} onClick={toggleMenu}><MdOutlineTipsAndUpdates size={20}/>Tips</a>
+        </div>
+      )}
     </div>
   );
 };

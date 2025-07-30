ac5cec1952f1b90f8eb352241089c9401f581d1c
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-07-30 14:13:14 +0300

    designed the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex container overflow-x-hidden flex-col p-5 text-white font-bold w-full items-center justify-center h-full lg:h-screen'>
-      <h1 className=" lg:text-4xl text-3xl font-bold text-center mb-2">
+    <div className='flex container overflow-x-hidden flex-col  text-gray-700 font-bold w-full items-center justify-center h-full lg:h-screen'>
+      <img src='/background1.jpg' alt="image" className='w-full h-auto'/>
+      <h1 className=" p-5 lg:text-4xl text-4xl font-bold text-center mb-2">
          Bringing Flavor, Flair & Full Event Magic
       </h1>
-      <h1 className="lg:text-4xl text-2xl text-orange-300 font-bold text-center mb-4">
+      <h1 className="lg:text-4xl px-5 text-3xl text-orange-300 font-bold text-center mb-4">
         To Your Special Day
       </h1>
-    <p className="lg:text-[19px] font-bold text-center text-white max-w-2xl mx-auto">
+    <p  className="px-5 lg:text-[19px] text-[19px] font-bold text-center text-gray-700 max-w-2xl mx-auto">
       From meals to music, tents to planning we handle it all so you can relax and enjoy your event.
     </p>
-    <button className='flex w-full lg:w-[300px] rounded-full items-center justify-center bg-orange-400 text-[13px] p-2 font-bold text-white mt-7 cursor-pointer'>Get In Touch</button>
+    <div className='w-full flex items-center justify-center px-5'>
+    <button className='flex w-full lg:w-[300px] rounded-full items-center justify-center bg-orange-400 text-[13px] p-2.5 font-bold text-white mt-7 cursor-pointer'>Get In Touch</button>
+    </div>
     </div>
   )
 }

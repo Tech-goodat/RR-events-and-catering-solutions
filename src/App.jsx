04175fabc38d4b1d8449b5baa8e167d87e2aed5e
Felix Kiprotich <felixkiprotich2000@gmail.com>
2025-07-30 12:30:26 +0300

    added a smooth transition to my page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Services from './components/Services'
-import Tips from './components/Tips'
-import NavBar from './components/NavBar'
-
-
+import React from 'react';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import Tips from './components/Tips';
 
 const App = () => {
   return (
-   <Router>
     <div className='flex flex-col w-full items-center justify-center'>
       <NavBar />
-      <Home />
-      <About />
-      <Contact />
-      <Services />
-      <Tips />
-    </div>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/services' element={<Services />} />
-      <Route path='/tips' element={<Tips />} />
-    </Routes>
-   </Router>
-  )
-}
 
-export default App
+      <section id='home' className='w-full pt-20'>
+        <Home />
+      </section>
+      <section id='about' className='w-full pt-20'>
+        <About />
+      </section>
+      <section id='contact' className='w-full pt-20'>
+        <Contact />
+      </section>
+      <section id='services' className='w-full pt-20'>
+        <Services />
+      </section>
+      <section id='tips' className='w-full pt-20'>
+        <Tips />
+      </section>
+    </div>
+  );
+};
+
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className='flex w-full items-center justify-between h-[50px] p-5 shadow-md'>
-        <Link to='/' className=' hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm items-center justify-center'>Home</Link>
-        <Link to='/about' className=' hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm items-center justify-center'>About</Link>
-        <Link to='/contact' className=' hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm items-center justify-center'>Contact</Link>
-        <Link to='/services' className=' hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm items-center justify-center'>Services</Link>
-        <Link to='/tips' className=' hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm items-center justify-center'>Tips</Link>
-        
+    <div className='flex fixed top-0 left-0 z-50 w-full items-center justify-between h-[50px] p-5 shadow-md'>
+      <a href='#home' className='hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm'>Home</a>
+      <a href='#about' className='hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm'>About</a>
+      <a href='#contact' className='hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm'>Contact</a>
+      <a href='#services' className='hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm'>Services</a>
+      <a href='#tips' className='hover:bg-orange-400 hover:text-white rounded-full px-6 p-1.5 text-sm'>Tips</a>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
